0f3c04dbfe73139bad0ac452ca4a0edac2b19bff
UnhappyWithoutU <gannatic.german@gmail.com>
2021-04-25 22:04:08 +0200

    init

--- src/startup.lua
+++ src/startup.lua
@@ -5,49 +5,49 @@
 --
 
 -- Get Monitor
-local monitor = peripheral.wrap("monitor_2")
-local Reactormonitor = peripheral.wrap("monitor_3")
-local memonitor = peripheral.wrap("monitor_4")
+local Monitor = peripheral.wrap("Monitor_2")
+local ReactorMonitor = peripheral.wrap("Monitor_3")
+local meMonitor = peripheral.wrap("Monitor_4")
 
 local outComputer = peripheral.wrap("computer_12")
 local modem = peripheral.find("modem")
 
 local button = require("button")
 local graphic = require("graphic")
--- I like trains
+
 -- Reset Monitor Function
 local function clearReactor()
-    Reactormonitor.clear()
-    Reactormonitor.setBackgroundColor(colors.black)
-    Reactormonitor.setTextScale(0.5)
-    local oldReactorterm = term.redirect(Reactormonitor)
-    graphic.drawBox(1,1,143,52,colors.gray,Reactormonitor)
+    ReactorMonitor.clear()
+    ReactorMonitor.setBackgroundColor(colors.black)
+    ReactorMonitor.setTextScale(0.5)
+    local oldReactorterm = term.redirect(ReactorMonitor)
+    graphic.drawBox(1,1,143,52,colors.gray,ReactorMonitor)
     Reactorimage = paintutils.loadImage("Reactor.nfp")
     paintutils.drawImage(Reactorimage,math.floor(((143 / 2)) - (114 / 2)), 6)
-    Reactormonitor.setBackgroundColor(colors.black)
+    ReactorMonitor.setBackgroundColor(colors.black)
     term.redirect(oldReactorterm)
 end
 
 local function clearMe()
-    memonitor.clear()
-    memonitor.setBackgroundColor(colors.black)
-    memonitor.setTextScale(0.5)
-    local oldmeterm = term.redirect(memonitor)
-    graphic.drawBox(1,1,143,52,colors.gray,memonitor)
+    meMonitor.clear()
+    meMonitor.setBackgroundColor(colors.black)
+    meMonitor.setTextScale(0.5)
+    local oldmeterm = term.redirect(meMonitor)
+    graphic.drawBox(1,1,143,52,colors.gray,meMonitor)
     meimage = paintutils.loadImage("Me.nfp")
     paintutils.drawImage(meimage,math.floor(((143 / 2)) - (42 / 2)), 6)
-    memonitor.setBackgroundColor(colors.black)
+    meMonitor.setBackgroundColor(colors.black)
     term.redirect(oldmeterm)
 end
 
 local function clearMonitor()
-    monitor.clear()
-    monitor.setBackgroundColor(colors.black)
-    monitor.setTextScale(0.5)
+    Monitor.clear()
+    Monitor.setBackgroundColor(colors.black)
+    Monitor.setTextScale(0.5)
     graphic.drawBox(1,1,164,52,colors.gray)
     image = paintutils.loadImage("Logo.nfp")
     paintutils.drawImage(image,math.floor(((164 / 2)) - (106 / 2)), 6)
-    monitor.setBackgroundColor(colors.black)
+    Monitor.setBackgroundColor(colors.black)
     -- left
     graphic.drawBox(19,24,49,29,colors.white)
     graphic.drawBox(19,32,49,37,colors.white)
@@ -57,38 +57,38 @@ local function clearMonitor()
     graphic.drawBox(54,32,84,37,colors.white)
 end
 
-local function setMonitorData(monitor)
-    monitor.setPaletteColour(colors.lime, 0xbf5f06)         -- button on
-    monitor.setPaletteColour(colors.red, 0x522700)          -- button off
-    monitor.setPaletteColour(colors.orange, 0xff891f)       -- logo
-    monitor.setPaletteColour(colors.gray, 0xff891f)         -- frame
-    monitor.setPaletteColour(colors.black, 0x18191a)        -- background
+local function setMonitorData(Monitorarg)
+    Monitorarg.setPaletteColour(colors.lime, 0xbf5f06)         -- button on
+    Monitorarg.setPaletteColour(colors.red, 0x522700)          -- button off
+    Monitorarg.setPaletteColour(colors.orange, 0xff891f)       -- logo
+    Monitorarg.setPaletteColour(colors.gray, 0xff891f)         -- frame
+    Monitorarg.setPaletteColour(colors.black, 0x18191a)        -- background
 end
 
 -- Dynamic Functions
 
-local function updateColor(state,x,y,x2,y2,text)
+local function updateColor(Monitorarg,state,x,y,x2,y2,text)
     if state then
         graphic.drawFilledBox(x,y,x2,y2,colors.lime)
-        monitor.setBackgroundColor(colors.lime)
+        Monitorarg.setBackgroundColor(colors.lime)
     else
         graphic.drawFilledBox(x,y,x2,y2,colors.red)
-        monitor.setBackgroundColor(colors.red)
+        Monitorarg.setBackgroundColor(colors.red)
     end
-    monitor.setCursorPos(math.floor((x + (30 / 2)) - (text:len() / 2)), y2-2)
-    monitor.setTextColor(colors.white)
-    monitor.write(text)
-    monitor.setBackgroundColor(colors.black)
+    Monitorarg.setCursorPos(math.floor((x + (30 / 2)) - (text:len() / 2)), y2-2)
+    Monitorarg.setTextColor(colors.white)
+    Monitorarg.write(text)
+    Monitorarg.setBackgroundColor(colors.black)
 end
 
 local function updateButtons()
     -- links
-    updateColor(redstone.getOutput("right"),20,25,48,28,"1 | Haupttür")
-    updateColor(redstone.getOutput("left"),20,33,48,36,"2 | Licht")
-    updateColor(redstone.getOutput("bottom"),20,41,48,44,"3 | Unknown")
+    updateColor(Monitor,redstone.getOutput("right"),20,25,48,28,"1 | Haupttür")
+    updateColor(Monitor,redstone.getOutput("left"),20,33,48,36,"2 | Licht")
+    updateColor(Monitor,redstone.getOutput("bottom"),20,41,48,44,"3 | Unknown")
     -- rechts
-    updateColor(redstone.getOutput("front"),55,25,83,28,"4 | Reactor")
-    updateColor(redstone.getOutput("top"),55,33,83,36,"5 | Unknown")
+    updateColor(Monitor,redstone.getOutput("front"),55,25,83,28,"4 | Reactor")
+    updateColor(Monitor,redstone.getOutput("top"),55,33,83,36,"5 | Unknown")
 end
 
 -- Button Functions
@@ -117,7 +117,7 @@ local function toggle5()
     updateButtons()
 end
 
-local oldTerm = term.redirect(monitor)         -- Monitor Output Start
+local oldTerm = term.redirect(Monitor)         -- Monitor Output Start
 
 clearMonitor()
 clearReactor()
@@ -131,16 +131,16 @@ local button3 = button.newButton(20,41,28,5,toggle3)    -- unknown
 local button4 = button.newButton(55,25,28,5,toggle4)    -- reactor
 local button5 = button.newButton(55,33,28,5,toggle5)    -- unknown
 
-setMonitorData(monitor)
-setMonitorData(Reactormonitor)
-setMonitorData(memonitor)
+setMonitorData(Monitor)
+setMonitorData(ReactorMonitor)
+setMonitorData(meMonitor)
 
 updateButtons()
 
 while true do
     local event = {os.pullEvent()}
     local newEvent = event
-    if event[1] == "monitor_touch" then
+    if event[1] == "Monitor_touch" then
         newEvent[1] = "mouse_click"
         newEvent[2] = 1
     end
